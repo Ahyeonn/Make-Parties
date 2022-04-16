a66b4f03bf05325cbe50e192bddca57be93e0e6c
Ahyeonn <ahyeon971030@gmail.com>
2022-04-16 15:16:53 -0700

    Users can delete rsvps

--- controllers/rsvps.js
+++ controllers/rsvps.js
@@ -17,6 +17,15 @@ module.exports = (app, models) => {
             console.log(err)
         });
     });
-    
+
     // DESTROY  
+      // DELETE
+    app.delete('/events/:eventId/rsvps/:id', (req, res) => {
+        models.Rsvp.findByPk(req.params.id).then(rsvp => {
+            rsvp.destroy();
+            res.redirect(`/events/${req.params.eventId}`);
+        }).catch((err) => {
+            console.log(err);
+        });
+    });
   }
